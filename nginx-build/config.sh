f2edf5834f5dc4d9bfb84f4a1eb6fb4ebf8579ba
huihuimoe <0w0@loli.pet>
2018-10-22 02:39:46 +0800

    update version

--- nginx-build/config.sh
+++ nginx-build/config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nginx_version=1.15.2
+nginx_version=1.15.5
 echo_nginx_module_version=0.61
 headers_more_nginx_module_version=0.33
 lua_nginx_module_version=0.10.13
@@ -8,6 +8,6 @@ pagespeed_ngx_version=1.13.35.2-stable
 fancyindex_version=0.4.2
 rtmp_module_version=1.2.1
 nchan_version=1.2.1
-openssl_version=1.1.1-pre8
+openssl_version=1.1.1
 pcre_version=8.42
 clang_version=6.0
