e2ea816623b0a21b6fc2e9805da901f8e235d2a9
huihuimoe <0w0@loli.pet>
2019-02-28 02:46:34 +0800

    chore: add ssl_ecdh_curve

--- nginx-build-with-boringssl/debian/sed.sh
+++ nginx-build-with-boringssl/debian/sed.sh
@@ -5,4 +5,6 @@ for file in $(ls debian/nginx-boringssl-huihui/usr/lib/nginx/modules) ; do
 done
 sed -i '/pcre_jit  on;/a\ ' debian/nginx-boringssl-huihui/etc/nginx/nginx.conf
 
-sed -i 's/#tcp_nopush     on;/tcp_nopush      on;\n    tcp_nodelay     on;\n    aio             on;\n\n    ssl_dyn_rec_enable on;/' debian/nginx-boringssl-huihui/etc/nginx/nginx.conf
+sed -i 's/#tcp_nopush     on;/tcp_nopush      on;\n    tcp_nodelay     on;\n    aio             on;\n\n    ssl_dyn_rec_enable on;\n    ssl_ecdh_curve prime256v1:secp384r1;/' debian/nginx-boringssl-huihui/etc/nginx/nginx.conf
+
+cp debian/nginx-boringssl-huihui/etc/nginx/nginx.conf debian/nginx-boringssl-huihui/etc/nginx/nginx.conf.default
