a078b647a5a0a0b6101e7c3f93dd925b0f0d424c
Hui Hui <0w0@loli.pet>
2021-07-18 22:13:41 +0800

    bump nginx to 1.21.1

--- nginx-build/config.sh
+++ nginx-build/config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-nginx_version=1.19.6
+nginx_version=1.21.1
 # https://github.com/openresty/echo-nginx-module/releases
 echo_nginx_module_version=0.62
 # https://github.com/openresty/headers-more-nginx-module/releases
 headers_more_nginx_module_version=0.33
 # https://github.com/openresty/lua-nginx-module/releases
-lua_nginx_module_version=0.10.19
+lua_nginx_module_version=0.10.20
 # https://github.com/vision5/ngx_devel_kit/releases
 ngx_devel_kit_version=0.3.1
 # https://github.com/apache/incubator-pagespeed-ngx/releases
@@ -13,25 +13,25 @@ pagespeed_ngx_version=1.14.33.1-RC1
 # https://github.com/aperezdc/ngx-fancyindex/releases
 fancyindex_version=0.5.1
 # https://github.com/arut/nginx-rtmp-module/releases
-rtmp_module_version=1.2.1
+rtmp_module_version=1.2.2
 # https://github.com/slact/nchan/releases
-nchan_version=1.2.7
+nchan_version=1.2.8
 # https://github.com/openssl/openssl/releases
-openssl_version=3.0.0-alpha10
+openssl_version=3.0.0-beta1
 # https://ftp.pcre.org/pub/pcre
-pcre_version=8.44
-clang_version=11
+pcre_version=8.45
+clang_version=12
 
 # https://github.com/openresty/luajit2/releases
-luajit2_version=2.1-20201229
+luajit2_version=2.1-20210510
 # https://github.com/openresty/stream-lua-nginx-module/releases
-stream_lua_nginx_module_version=0.0.9
+stream_lua_nginx_module_version=0.0.10
 # https://github.com/leev/ngx_http_geoip2_module/releases
 ngx_http_geoip2_module_version=3.3
 # https://github.com/openresty/lua-resty-core/releases
-nginx_lua_resty_core_version=0.1.21
+nginx_lua_resty_core_version=0.1.22
 # https://github.com/openresty/lua-resty-lrucache/releases
-nginx_lua_resty_lrucache_version=0.10
+nginx_lua_resty_lrucache_version=0.11
 # https://github.com/ekho/lua-nginx-split-clients/releases
 nginx_lua_split_clients_version=0.0.2
 # https://github.com/jemalloc/jemalloc/releases
