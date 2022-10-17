a153377176498ae211462c7ec2d660cf9e4c0006
huihuimoe <0w0@loli.pet>
2022-10-17 16:07:28 +0800

    chore: nginx 1.23.1

--- nginx-build-with-boringssl/config.sh
+++ nginx-build-with-boringssl/config.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
-nginx_version=1.21.6
-# https://github.com/openresty/echo-nginx-module/releases
+nginx_version=1.23.1
+# https://github.com/openresty/echo-nginx-module/tags
 echo_nginx_module_version=0.62
-# https://github.com/openresty/headers-more-nginx-module/releases
-headers_more_nginx_module_version=0.33
-# https://github.com/openresty/lua-nginx-module/releases
-lua_nginx_module_version=0.10.21rc1
-# https://github.com/vision5/ngx_devel_kit/releases
+# https://github.com/openresty/headers-more-nginx-module/tags
+headers_more_nginx_module_version=0.34
+# https://github.com/openresty/lua-nginx-module/tags
+lua_nginx_module_version=0.10.22
+# https://github.com/vision5/ngx_devel_kit/tags
 ngx_devel_kit_version=0.3.1
-# https://github.com/apache/incubator-pagespeed-ngx/releases
+# https://github.com/apache/incubator-pagespeed-ngx/tags
 pagespeed_ngx_version=1.14.33.1-RC1
-# https://github.com/aperezdc/ngx-fancyindex/releases
+# https://github.com/aperezdc/ngx-fancyindex/tags
 fancyindex_version=0.5.2
-# https://github.com/arut/nginx-rtmp-module/releases
+# https://github.com/arut/nginx-rtmp-module/tags
 rtmp_module_version=1.2.2
-# https://github.com/slact/nchan/releases
-nchan_version=1.2.15
-# https://github.com/openssl/openssl/releases
+# https://github.com/slact/nchan/tags
+nchan_version=1.3.4
+# https://github.com/openssl/openssl/tags
 # openssl_version=1.1.1i
 # https://ftp.pcre.org/pub/pcre
 pcre_version=8.45
 clang_version=12
 
-# https://github.com/openresty/luajit2/releases
-luajit2_version=2.1-20220111
-# https://github.com/openresty/stream-lua-nginx-module/releases
-stream_lua_nginx_module_version=0.0.11rc1
-# https://github.com/leev/ngx_http_geoip2_module/releases
-ngx_http_geoip2_module_version=3.3
-# https://github.com/openresty/lua-resty-core/releases
-nginx_lua_resty_core_version=0.1.23rc1
-# https://github.com/openresty/lua-resty-lrucache/releases
-nginx_lua_resty_lrucache_version=0.11
-# https://github.com/ekho/lua-nginx-split-clients/releases
+# https://github.com/openresty/luajit2/tags
+luajit2_version=2.1-20220915
+# https://github.com/openresty/stream-lua-nginx-module/tags
+stream_lua_nginx_module_version=0.0.11
+# https://github.com/leev/ngx_http_geoip2_module/tags
+ngx_http_geoip2_module_version=3.4
+# https://github.com/openresty/lua-resty-core/tags
+nginx_lua_resty_core_version=0.1.24
+# https://github.com/openresty/lua-resty-lrucache/tags
+nginx_lua_resty_lrucache_version=0.13
+# https://github.com/ekho/lua-nginx-split-clients/tags
 nginx_lua_split_clients_version=0.0.2
-# https://github.com/jemalloc/jemalloc/releases
-jemalloc_version=5.2.1
-# https://github.com/ivmai/libatomic_ops/releases
-libatomic_ops_version=7.6.12
+# https://github.com/jemalloc/jemalloc/tags
+jemalloc_version=5.3.0
+# https://github.com/ivmai/libatomic_ops/tags
+libatomic_ops_version=7.6.14
--- nginx-build/debian/sed.sh
+++ nginx-build/debian/sed.sh
@@ -6,6 +6,7 @@ sed -i '/logs\/nginx.pid;/a\ \npcre_jit  on;' debian/nginx-huihui/etc/nginx/ngin
 sed -i '/pcre_jit  on;/a\ ' debian/nginx-huihui/etc/nginx/nginx.conf
 
 sed -i 's/#tcp_nopush     on;/tcp_nopush      on;\n    tcp_nodelay     on;\n    aio             on;\n\n    ssl_dyn_rec_enable on;\n    ssl_ecdh_curve prime256v1:secp384r1;/' debian/nginx-huihui/etc/nginx/nginx.conf
+sed -i 's/keepalive_timeout  65;/keepalive_timeout 65;\n    keepalive_requests 4294967295;\n    reset_timedout_connection on;/' debian/nginx-huihui/etc/nginx/nginx.conf
 
 # GeoIP
 sed -i 's/include       mime.types;/geoip_country \/usr\/share\/GeoIP\/GeoIPv6.dat;\n    #geoip_city    \/usr\/share\/GeoIP\/GeoIPCity.dat;\n    #geoip_org     \/usr\/share\/GeoIP\/GeoIPASNum.dat;\n    include       cloudflare-geo.conf;\n    include       mime.types;/' debian/nginx-huihui/etc/nginx/nginx.conf
